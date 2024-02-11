068d10674234e090bd3363676f57c008de0daf98
ernesto.quispe <ernesto.cajan@gmail.com>
2024-02-10 23:57:17 -0300

    Chamber of Commerce Home Page Continued

--- chamber/scripts/slider.js
+++ chamber/scripts/slider.js
@@ -1,8 +1,16 @@
-const modeButton = document.querySelector(".switch");
 const main = document.querySelector("main");
 const checkbox = document.querySelector('#toggleCheckbox');
+
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const menu = document.querySelector("#menu");
+const footer = document.querySelector("footer");
 
 checkbox.addEventListener('change', function() {
 
     main.classList.toggle('dark');
+    header.classList.toggle('dark');
+    nav.classList.toggle('dark');
+    menu.classList.toggle('dark-menu');
+    footer.classList.toggle('dark');
   });
