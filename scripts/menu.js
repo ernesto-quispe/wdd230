643fd38038e6ddb6c7a24bd7f326aa0530619feb
joshowen04 <97120374+joshowen04@users.noreply.github.com>
2024-02-11 16:24:30 -0300

    Update menu.js
    Navlinks

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,8 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-
+const navLinks = document.querySelector('.navLinks');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+	navLinks.classList.toggle('open');
 });
