3e99efe751cc64911b20ce8b61c3c9608c7f6c03
ernesto.quispe <ernesto.cajan@gmail.com>
2024-02-17 23:52:01 -0300

    Responsive Images

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,18 @@
+let current = document.querySelector(".currentTemp").textContent;
+let wind = document.querySelector(".windSpeed").textContent;
+let windChill = document.querySelector(".windChill");
+
+function windchill(temp,windspeed) {
+    let chill = ""
+
+    if (temp <= 50 && windspeed > 3){
+        chill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * Math.pow(windspeed, 0.16)
+        chill = `${Math.round(chill,1)} &deg F`
+        console.log(chill)
+    }
+    else {
+        chill = 'N/A'
+    }
+    document.querySelector(".windChill").innerHTML = chill
+}
+windchill(current,wind);
