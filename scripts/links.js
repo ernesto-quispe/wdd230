9d593998d2b518278d489a88d44f5766e85751b7
ernesto.quispe <ernesto.cajan@gmail.com>
2024-03-08 01:37:45 -0300

    JSON Menu

--- scripts/links.js
+++ scripts/links.js
@@ -1,42 +1,48 @@
 const baseURL = "https://ernesto-quispe.github.io/wdd230/";
 
-// const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
-const linksURL = "data/links.json";
+const linksURL = "https://ernesto-quispe.github.io/wdd230/data/links.json";
+//const linksURL = "data/links.json";
 
 async function getLinks(linksURL) {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data.lessons[0].links);
-    displayLinks(data);
-  }
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  const lessons = data.lessons;
+  displayLinks(lessons);
+}
 
-  getLinks(linksURL)
-  
-  function displayLinks(data) {
-    let card = document.querySelector('#links')
-    let h3 = document.createElement('h3');
-    let ul = document.createElement('ul');
+getLinks(linksURL);
 
-    data.forEach((lesson) => {
-     
-        lesson.forEach((link) => {
+function displayLinks(lessons) {
+  let card = document.querySelector("#links");
+  let h3 = document.createElement("h3");
+  let ul = document.createElement("ul");
+  h3.textContent = "Learning Activities";
+  lessons.forEach((lesson) => {
+    let li = document.createElement("li");
+    const week = `Week ${lesson.lesson}: `;
+    let weekTitle = document.createElement("h4")
+    weekTitle.textContent = week
+    li.appendChild(weekTitle);
+    console.log(week);
+    const links = lesson.links;
+    console.log(links);
 
-            let li = document.createElement('li');
-            let a = document.createElement('a');
-                    
-            a.setAttribute('href', link.url);
-            a.setAttribute('target', '_blank'); 
-            
-            li.textContent = `${link.title}`;
-            // Append the section(card) with the created elements
-            li.appendChild(a); //fill in the blank
-    
-        
-            ul.appendChild(li);
-          }); // end of arrow function and forEach loop
+    links.forEach((link,index,arr) => {
 
-    });
+      let a = document.createElement("a");
+      let span = document.createElement("span");
+      span.textContent = " | "
+      a.textContent = link.title
+      a.setAttribute("href", link.url);
+      a.setAttribute("target", "_blank");
+      li.appendChild(a); 
+      if (index != arr.length - 1) {
+        li.appendChild(span);
+      }
+    }); 
 
-      card.appendChild(ul)
-
-  }
+    ul.appendChild(li);
+    card.appendChild(h3);
+    card.appendChild(ul);
+  });
+}
