ba36ed8e75d8fa6e3f48ba33d1486aeaf35b0c7f
joshowen04 <owe21015@byui.edu>
2024-03-08 18:13:04 -0300

    directory html

---
+++ chamber/scripts/members.js
@@ -0,0 +1,60 @@
+const membersURL = "https://ernesto-quispe.github.io/wdd230/chamber/data/members.json";
+
+//const membersURL = "./data/members.json";
+
+
+async function getMembers(membersURL) {
+  const response = await fetch(membersURL);
+  const data = await response.json();
+  displayLinks(data);
+}
+
+
+function displayMembers(data) {
+    const container = document.querySelector('#directory-list');
+    container.innerHTML = ''; // Clear existing content
+  
+    data.businesses.forEach(business => {
+      const card = document.createElement('section');
+      card.classList.add('card');
+  
+      const logo = document.createElement('img');
+      logo.setAttribute('src', business.logoname);
+      logo.setAttribute('alt', business.name)
+      card.appendChild(logo);
+  
+      const name = document.createElement('h3');
+      name.textContent = business.name;
+      card.appendChild(name);
+  
+      const owner = document.createElement('p');
+      owner.textContent = 'Owner: ' + business.owner;
+      card.appendChild(owner);
+  
+      const founded = document.createElement('p');
+      founded.textContent = 'Founded: ' + business.founded;
+      card.appendChild(founded);
+  
+      const address = document.createElement('p');
+      address.textContent = 'Address: ' + business.address;
+      card.appendChild(address);
+  
+      const telephone = document.createElement('p');
+      telephone.textContent = 'Telephone: ' + business.telephone;
+      card.appendChild(telephone);
+  
+      const membership = document.createElement('p');
+      membership.textContent = 'Membership: ' + business.membership;
+      card.appendChild(membership);
+  
+      const link = document.createElement('a');
+      link.href = business.url;
+      link.textContent = 'Visit Website';
+      card.appendChild(link);
+  
+      container.appendChild(card);
+    });
+  }
+  
+  getMembers('members.json'); // Replace 'members.json' with the path to your JSON file
+  
