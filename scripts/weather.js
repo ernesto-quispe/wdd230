6c9cf3542a10a671e971da9b39b206b31b549988
ernesto.quispe <ernesto.cajan@gmail.com>
2024-03-15 01:10:59 -0300

    Course Home Enhancement

---
+++ scripts/weather.js
@@ -0,0 +1,39 @@
+// select HTML elements in the document
+  const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-33.41&lon=-70.51&units=imperial&appid=01c651e5d4e8eacfe1ceb1611733c463';
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('#caption');
+
+
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); 
+        displayResults(data); 
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconCode = data.weather[0].icon;
+  const iconSrc = `https://openweathermap.org/img/w/${iconCode}.png`;
+  let desc = data.weather[0].description;
+
+
+  weatherIcon.setAttribute('src', iconSrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+
+
